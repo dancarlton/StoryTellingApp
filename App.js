03433f7daa9fbf6754610cbcf033e45fd804cdd9
Dan Carlton <danlouis.carlton@gmail.com>
2023-08-10 19:38:25 -0700

    backend

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs');
 
 // Handlebars
 app.engine(
@@ -39,6 +39,7 @@ app.engine(
       formatDate,
       stripTags,
       truncate,
+      editIcon,
     },
     defaultLayout: 'main',
     extname: '.hbs',
@@ -60,6 +61,12 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
